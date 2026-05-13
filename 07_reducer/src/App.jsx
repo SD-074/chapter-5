81326a93c74542286bac40ab5a6bb6902b4a2090
KhaledAlabssi <khaled.absi5@gmail.com>
2026-05-13 16:58:29 +0200

    useReducer complete version

--- 07_reducer/src/App.jsx
+++ 07_reducer/src/App.jsx
@@ -2,17 +2,15 @@ import { useReducer } from "react";
 import cartReducer from "./reducers/cartReducer";
 
 const App = () => {
-  // const initialState = { items: [], total: 0, count: 0 };
-  const initialState = [];
 
+  const initialState = { items: [], total: 0, itemsCount: 0 };
   const products = [
-    { id: 1, name: "Product 1", price: 29.99 },
+    { id: 1, name: "Product 1", price: 29.99 }, // quantity => cartItemsQ && total => quantity * price
     { id: 2, name: "Product 2", price: 49.99 },
     { id: 3, name: "Product 3", price: 19.99 },
   ];
 
   const [cart, dispatch] = useReducer(cartReducer, initialState);
-
 
   return (
     <div className="main-container">
@@ -29,11 +27,11 @@ const App = () => {
       ))}
       <section>
         <h3>Cart Items | total: </h3>
-        {cart.length}
-        {cart.length > 0 &&
-          cart.map((cartItem) => (
+        {cart.items.length}
+        {cart.items.length > 0 &&
+          cart.items.map((cartItem) => (
             <p>
-              {cartItem.name}{" "}
+              {cartItem.name} | {cartItem.price} | {cartItem.quantity}
               <button
                 className="btn"
                 onClick={() => dispatch({ type: "delete", payload: cartItem })}
@@ -43,12 +41,14 @@ const App = () => {
             </p>
           ))}
       </section>
-      {cart.length > 0 && <button
-            className="btn"
-            onClick={() => dispatch({ type: "clear" })}
-          >
-            clear Cart
-          </button>}
+      {cart.items.length > 0 && (
+        <button className="btn" onClick={() => dispatch({ type: "clear" })}>
+          clear Cart
+        </button>
+      )}
+      <div>
+        Cart info: {cart.total} | {cart.itemsCount}
+      </div>
     </div>
   );
 };
--- 07_reducer/src/reducers/cartReducer.js
+++ 07_reducer/src/reducers/cartReducer.js
@@ -1,25 +1,52 @@
+function calculation(cart) {
+  const reduceResult = cart.items.reduce(
+    (acc, item) => {
+      acc.cartItemsQ = acc.cartItemsQ += item.quantity;
+      acc.total = acc.total += item.quantity * item.price;
+
+      return acc;
+    },
+    { cartItemsQ: 0, total: 0 },
+  );
+
+  console.log(reduceResult);
+
+  cart.total = reduceResult.total;
+  cart.itemsCount = reduceResult.cartItemsQ;
+  console.log(cart);
+
+  return cart;
+}
+
+export default function cartReducer(state, action) {
+  const { type, payload } = action;
 
 
+  if (action.type == "g") {
+    // if exist in cart: we need to only increase the item quantity...
+    const isExist = state.items.find((i) => i.name == payload.name);
 
+    if (isExist) {
+      const items = state.items.map((i) => {
+        if (i.name == isExist.name) {
+          return { ...i, quantity: isExist.quantity + 1 };
+        } else return i;
+      });
 
+      state = { ...state, items };
+    } else {
+      const items = [...state.items, { ...payload, quantity: 1 }];
 
-
-export default  function cartReducer(state, action) {
-    const { type, payload } = action;
-    console.log("cartReducer function been called!");
-    console.log("with type: ", type);
-    console.log("with payload: ", payload);
-    //push, [...array]
-    const newCart = [...state, payload];
-
-    if(action.type == "g"){
-      state = [...state, payload]
-
-    } else if (action.type == "delete"){
-      state = state.filter(i => i.name != payload.name)
-    }else if (action.type == "clear"){
-      state = []
+      state = { ...state, items };
     }
-
-    return state;
+  } else if (action.type == "delete") {
+    const items = state.items.filter((i) => i.name != payload.name);
+    state = { ...state, items };
+  } else if (action.type == "clear") {
+    const items = [];
+    state = { ...state, items };
   }
+  state = calculation(state);
+
+  return state;
+}
